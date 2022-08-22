cf66c7d5ea6655438ccec20bee63e72cf135c1d4
rabia öztürk <Github rabia1479oz@email.com>
2022-08-22 16:57:23 +0300

    proje eklendi.

---
+++ src/MMain.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class MMain {
+    public static void main(String[] args) {
+        //değişkenleri oluştur
+        int mat,fizik,kimya,tr,tarih,muzik;
+
+        //Scanner sınıfını tanımladık
+        Scanner inp =new Scanner(System.in);
+
+        //kullanıcıdan değerleri al
+        System.out.println("Matematik notunuz: ");
+        mat = inp.nextInt();
+
+
+        System.out.println("Fizik notunuz: ");
+        fizik = inp.nextInt();
+
+        System.out.println("Kimya notunuz: ");
+        kimya = inp.nextInt();
+
+        System.out.println("Türkçe notunuz: ");
+        tr = inp.nextInt();
+
+        System.out.println("Tarih notunuz: ");
+        tarih = inp.nextInt();
+
+        System.out.println("Müzik notunuz: ");
+        muzik = inp.nextInt();
+
+
+        int toplam = (mat + fizik + kimya + tr + tarih + muzik);
+        double sonuc = toplam / 6.0;
+
+        System.out.println("Ortalamanız : " + sonuc);
+
+        boolean kosul1 = sonuc>=60;
+        boolean kosul2 = sonuc<60;
+
+
+        String str = kosul1 ? "Geçtiniz" : "Kaldınız";
+        System.out.println(str);
+
+
+
+
+
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,47 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.print("BİRİNCİ MAKALE \n");
+        byte vByte = 14;
+        System.out.println(vByte);
+
+        float vFloat = 2.8f;
+        System.out.println(vFloat);
+
+        char vChar = 't';
+        char vvchar = 106;
+        System.out.println(vChar);
+        System.out.println(vvchar);
+
+        boolean durum1 = true;
+        System.out.println(durum1);
+
+        String VsTR = "RABİA";
+        System.out.println(VsTR);
+
+        boolean sonuc = 5 > 1;
+        System.out.println(sonuc);
+
+        int a=3 ,b=5;
+        boolean res = a==b;
+        System.out.println(res);
+
+        int d=5 , e=5 , c=3;
+        boolean kosul1 = d==e ;
+        boolean kosul2 = e>c;
+        boolean so = kosul2 && kosul1;
+
+        String str = so ?  "doğru" : "değil";
+        int result = so ? 1 : 0;
+
+        System.out.println(str);
+
+        byte p = 0;
+        int u = p++;
+        System.out.print(u);
+
+
+
+
+    }
+}
+
