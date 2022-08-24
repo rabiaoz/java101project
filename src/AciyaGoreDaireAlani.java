4e767089887d3e9d69178526df7f51f8520a4b4b
rabia öztürk <Github rabia1479oz@email.com>
2022-08-24 16:37:56 +0300

    programlar eklendi.

---
+++ src/AciyaGoreDaireAlani.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AciyaGoreDaireAlani {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double r, alpha, S, pi=3.14;
+
+        System.out.print("Yarıçapı Giriniz: ");
+        r = input.nextDouble();
+
+        System.out.print("Merkez Açıyı Giriniz: ");
+        alpha = input.nextDouble();
+
+        S = (pi * r * r) * alpha / 360;
+
+        System.out.print("Daire Diliminin Alanı: " + S);
+
+    }
+}
--- src/DaireAlaniVeCevresi.java
+++ src/DaireAlaniVeCevresi.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class DaireAlaniVeCevresi {
+    public static void main(String[] args) {
+
+        double r, S, CVR, pi=3.14;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Yarıçapı Giriniz: ");
+        r = input.nextDouble();
+
+        CVR = 2 * pi * r;
+        S = pi * r * r;
+
+        System.out.println("Dairenin Alanı: " + S);
+        System.out.println("Dairenin Çevresi: " + CVR);
+
+    }
 }
