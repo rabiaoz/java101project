36270249b6ca1215edae1046a5342838da250079
rabia öztürk <Github rabia1479oz@email.com>
2022-08-30 01:44:38 +0300

    FİLE changed.

--- src/SayiSiralama.java
+++ src/SayiSiralama.java
@@ -11,22 +11,22 @@ public class SayiSiralama {
         System.out.println("c sayısını giriniz: ");
         c = input.nextInt();
 
-        if (a>b && a>c) {
-            if (b > c) {
-                System.out.println("a>b>c");
+        if (a<b && a<c) {
+            if (b < c) {
+                System.out.println("a<b<c");
             } else {
-                System.out.println("a>c>b");
+                System.out.println("a<c<b");
             }
-        }else if ((b>c) && (b>a)) {
-            if (a > c) {
-                System.out.println("b>a>c");
+        }else if ((b<c) && (b<a)) {
+            if (a < c) {
+                System.out.println("b<a<c");
             } else {
-                System.out.println("b>c>a");
+                System.out.println("b<c<a");
             }
-        } else if (b>a) {
-                System.out.println("c>b>a");
+        } else if (b<a) {
+                System.out.println("c<b<a");
             } else {
-                System.out.println("c>a>b");
+                System.out.println("c<a<b");
             }
     }
 }
