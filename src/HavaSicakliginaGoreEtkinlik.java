c0af71d0f1ae8627029e19cf49df816f67dfa667
rabia öztürk <Github rabia1479oz@email.com>
2022-08-29 23:09:31 +0300

    FİLE ADDED.

--- src/HavaSicakliginaGoreEtkinlik.java
+++ src/HavaSicakliginaGoreEtkinlik.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class HavaSicakliginaGoreEtkinlik {
+    public static void main(String[] args) {
+        int heat;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Hava sıcaklığını giriniz: ");
+        heat = input.nextInt();
+
+        if (heat<=5) System.out.println("Kayağa gidebilirsiniz.");
+        if (heat>=5 && heat<=15) System.out.println("Sinemaya gidebilirsiniz.");
+        if (heat>=15 && heat<=25) System.out.println("Pikniğe gidebilirsiniz.");
+        if (heat>=25) System.out.println("Yüzmeye gidebilirsiniz.");
+    }
 }
