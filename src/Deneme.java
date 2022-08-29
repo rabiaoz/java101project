5402692f2a53b9cfc9a63ba8432f28d64666900a
rabia öztürk <Github rabia1479oz@email.com>
2022-08-29 14:50:14 +0300

    commit messages.

---
+++ src/Deneme.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Deneme {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+
+        if ((n % 2 ==1) || ((n>=6)&&(n<=20))){
+            System.out.println("weird");
+        }
+        else {
+            System.out.println("not weird");
+        }
+
+    }
+}
+
