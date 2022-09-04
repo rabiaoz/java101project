fc1ded10840bb558b98959bc38b677e2b55b47fe
rabia öztürk <Github rabia1479oz@email.com>
2022-09-04 23:03:02 +0300

    file edited.

--- src/CinZodyagi.java
+++ src/CinZodyagi.java
@@ -8,32 +8,37 @@ public class CinZodyagi {
         System.out.print("Doğum yılınızı giriniz :");
         year = input.nextInt();
 
-        if (year % 12 == 0){
-            burc = "Maymun";
-        }else if (year % 12 == 1){
-            burc = "Horoz";
-        }else if (year % 12 == 2){
-            burc = "Köpek";
-        }else if (year % 12 == 3){
-            burc = "Domuz";
-        }else if (year % 12 == 4){
-            burc = "Fare";
-        }else if (year % 12 == 5){
-            burc = "Öküz";
-        }else if (year % 12 == 6){
-            burc = "Kaplan";
-        }else if (year % 12 == 7){
-            burc = "Tavşan";
-        }else if (year % 12 == 8){
-            burc = "Ejderha";
-        }else if (year % 12 == 9){
-            burc = "Yılan";
-        }else if (year % 12 == 10){
-            burc = "At";
-        }else if (year % 12 == 11){
-            burc = "Koyun";
-        }
+        if (year<0){
+            System.out.println("Hatalı giriş yaptınız!!!");
+        }else {
 
-        System.out.println("Çin Zodyağı Burcunuz: " + burc);
+            if (year % 12 == 0) {
+                burc = "Maymun";
+            } else if (year % 12 == 1) {
+                burc = "Horoz";
+            } else if (year % 12 == 2) {
+                burc = "Köpek";
+            } else if (year % 12 == 3) {
+                burc = "Domuz";
+            } else if (year % 12 == 4) {
+                burc = "Fare";
+            } else if (year % 12 == 5) {
+                burc = "Öküz";
+            } else if (year % 12 == 6) {
+                burc = "Kaplan";
+            } else if (year % 12 == 7) {
+                burc = "Tavşan";
+            } else if (year % 12 == 8) {
+                burc = "Ejderha";
+            } else if (year % 12 == 9) {
+                burc = "Yılan";
+            } else if (year % 12 == 10) {
+                burc = "At";
+            } else if (year % 12 == 11) {
+                burc = "Koyun";
+            }
+
+            System.out.println("Çin Zodyağı Burcunuz: " + burc);
+        }
     }
 }
