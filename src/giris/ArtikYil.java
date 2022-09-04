54f84de719f72afe33a11698e871e2dc57243b53
rabia öztürk <Github rabia1479oz@email.com>
2022-09-04 23:28:03 +0300

    file's places changed.

---
+++ src/giris/ArtikYil.java
@@ -0,0 +1,11 @@
+package giris;
+import java.util.Scanner;
+
+public class ArtikYil {
+    public static void main(String[] args) {
+        int year;
+
+        Scanner input = new Scanner(System.in);
+
+    }
+}
--- src/giris/ax.java
+++
@@ -1,4 +0,0 @@
-package giris;
-
-public class ax {
-}
