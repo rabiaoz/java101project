7a44a594484c88ba759146d5b4abcaaba3ea9fe6
rabia öztürk <Github rabia1479oz@email.com>
2022-09-06 16:49:26 +0300

    file commit.

--- src/giris/ArtikYil.java
+++ src/giris/ArtikYil.java
@@ -7,5 +7,22 @@ public class ArtikYil {
 
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Yıl Giriniz: ");
+        year = input.nextInt();
+
+        if (year % 100 == 0){
+            if (year % 400 == 0){
+                System.out.println(year + " artık bir yıldır.");
+            } else {
+                System.out.println(year + " artık bir yıl değildir.");
+            }
+        }else if (year % 4 == 0){
+            System.out.println(year + " artık  bir yıldır.");
+        } else {
+            System.out.println(year + " artık bir yıl değildir.");
+        }
+
+
+
     }
 }
