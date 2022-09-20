09a2c2c869e2df61f73700277499c0c27a93e9f0
rabia öztürk <Github rabia1479oz@email.com>
2022-09-20 17:38:47 +0300

    file added.

---
+++ src/giris/MukemmelSayi.java
@@ -0,0 +1,25 @@
+package giris;
+import java.util.Scanner;
+
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        int number,i,total=0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("sayı giriniz: ");
+        number = input.nextInt();
+
+        for (i=1;i<number;i++){
+            if (number % i == 0){
+                total += i;
+            }
+
+        }
+        if (total == number){
+            System.out.println(number + " mükemmel sayıdır.");
+        }else {
+            System.out.println(number + " mükemmel sayı değildir.");
+        }
+
+
+    }
+}
