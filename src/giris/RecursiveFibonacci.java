0a8fadc5ec5faca756e5a0d3fe61ed6862133621
rabia öztürk <Github rabia1479oz@email.com>
2022-09-25 23:27:00 +0300

    file added.

---
+++ src/giris/RecursiveFibonacci.java
@@ -0,0 +1,15 @@
+package giris;
+
+public class RecursiveFibonacci {
+    static int fib(int n){
+        if (n == 1 || n == 2){
+            return 1;
+        }
+        return fib(n - 2) + fib(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+
+    }
+}
